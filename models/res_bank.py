594390ad3c61bfe044c3fd6f9799cba167d6fe6c
Pedro Porras <pedro.porras@nmksoftware.co>
2021-10-22 22:14:48 -0500

    [update] Se modifica la etiqueta de los campos

--- models/res_bank.py
+++ models/res_bank.py
@@ -7,4 +7,4 @@ from odoo import models, fields, api
 class ResBank(models.Model):
     _inherit = 'res.bank'
 
-    city_id = fields.Many2one('res.city', string="City")
+    city_id = fields.Many2one('res.city', string="City of Address")
